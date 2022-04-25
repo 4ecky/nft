17ffc0dfe6894c9ddcf3dbf8fe2d4e5b9d132a34
Daniil Bondarenko <69973047+4ecky@users.noreply.github.com>
2022-04-25 10:12:05 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
-const swiper = new Swiper('.swiper',{
-
-  slidesPerView: 4,
-  spaceBetween: 40,
-  slidesPerGroup: 1,
+const swiper = new Swiper('.swiper', {
+  spacebetween: 40,
   loop: true,
   speed: 2000,
   autoplay: {
@@ -10,57 +7,67 @@ const swiper = new Swiper('.swiper',{
     stopOnLastSlide: false,
     disableOnInteraction: false,
   },
+  breakpoints: {
+    1310: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 3.8,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    767: {
+      slidesPerView: 2.2,
+    },
+    675: {
+      slidesPerView: 2,
+    },
+    575: {
+      slidesPerView: 1.8,
+    },
+    500: {
+      slidesPerView: 1.6,
+    },
+    450: {
+      slidesPerView: 1.4,
+    },
+    320: {
+      slidesPerView: 1,
+    },
+  },
 });
-// МЕНЮ БУРГЕР
-// 1. Мы должны получить нашу иконку
-const iconMenu = document.querySelector('.header__burger');
-// 2. Проверка на всякий пожарный
-// 3. В условии делаем тоже, что и в первом пункте, только вместо .menu__icon получаем в константу .menu__body (53:56 - перенесли)
-const menuBody = document.querySelector('.header__menu');
-if (iconMenu) {
-  // 4. создаем событие клик по иконке
-  iconMenu.addEventListener('click', function (e) {
-    //5. Обращаюсь к иконке, добавляю класс, который будет то появлятся(при нажатии на иконку) то пропадать(при закрытии, т.е. втором нажатии)
-    iconMenu.classList.toggle('_active');
-    // 6. Тоже самое делаем для menuBody, что и для iconMenu
-    menuBody.classList.toggle('_active');
-    // 7. Обращаемся к body и добавляем/убираем класс _lock, чтобы при открытом меню не скролилось
-    document.body.classList.toggle('_lock');
-  });
+
+// Меню бургер
+const burger = document.querySelector('.burger');
+const menuNav = document.querySelector('.menu');
+const navLink = document.querySelectorAll('.menu__list-link');
+
+burger.addEventListener('click', mobileMenu);
+
+function mobileMenu() {
+  burger.classList.toggle('_active');
+  menuNav.classList.toggle('_open');
 }
 
+// при нажатии на ссылку, меню закрывается
+navLink.forEach(n => n.addEventListener('click', closeMenu));
 
-// Прокрутка при клике
-const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
-if (menuLinks.length > 0) {
-  menuLinks.forEach(menuLink => {
-    menuLink.addEventListener('click', onMenuLinkClick);
-  });
+function closeMenu() {
+  burger.classList.toggle('_active');
+  menuNav.classList.toggle('_open');
+}
 
-  // Пишем функцию onMenuLinkClick
-  function onMenuLinkClick(e) {
-    const menuLink = e.target;
-    // условие
-    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-      const gotoBlock = document.querySelector(menuLink.dataset.goto);
-      // высчитываем положение объекта с учетом высоты шапки.
-      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+// плавная прокрутка к разделам
+const anchors = document.querySelectorAll('a[href*="#"]');
 
-      //  при клике  на раздел, меню пропадает и страница переводит на выбранный пункт
-      // 1.Если объект иконки меню содержит класс _active, т.е. фактически это означает, что меню открыто, то в этот момент:
-      if (iconMenu.classList.contains('_active')) {
-        // 2. мы должны убрать классы, которые мы добавляем при открытии меню (заменить toggle на remove)
-        iconMenu.classList.remove('_active');
-        menuBody.classList.remove('_active');
-        document.body.classList.remove('_lock');
-      }
-
-      // кусочек кода, который заставит скролл прокрутиться к нужному месту
-      window.scrollTo({
-        top: gotoBlockValue,
-        behavior: 'smooth'
-      });
-      e.preventDefault();
-    }
-  }
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (event) {
+    event.preventDefault();
+    const blockID = anchor.getAttribute('href');
+    document.querySelector('' + blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    })
+  })
 }
